f7d709a655d342e3b002405edad3d24417847778
Sahil <sahibhardwaj0802@gmail.com>
2021-11-19 23:46:03 -0500

    - Added some git comments in index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Javascript Activated!");
+
+// Git Commands - 
+// git clone <url>
+// git add <filename> 
+// git commit -a "message" or git commit -am "message"
+// git push is used to make the changes visible online!
+// git checkout -b <branchname> - create a new branch
+// git checkout <branchname> - go to that branch
+// git log - helps to check log
+// git staus - helps to check status
+// morge conflicts are resolved by talking to the other programmer!
+// fork is used to contribute to other github publicaly available projects
+// cd is used to change directory
+// open - github.com/new to make a new respository
